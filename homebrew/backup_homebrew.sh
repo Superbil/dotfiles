0526c2b1c626f3cf2e3c9a223a601c981753803d
Superbil <superbil@gmail.com>
2015-01-30 01:06:08 +0800

    Don't need to hard code path, let it get from env

--- homebrew/backup_homebrew.sh
+++ homebrew/backup_homebrew.sh
@@ -6,5 +6,5 @@ echo ""
 
 brew list | while read item;
 do
-    echo "install $item $(brew info $item | /usr/bin/grep 'Built from source with:' | /usr/bin/sed 's/^[ \t]*Built from source with:/ /g; s/\,/ /g')"
+    echo "install $item $(brew info $item | grep 'Built from source with:' | sed 's/^[ \t]*Built from source with:/ /g; s/\,/ /g')"
 done
