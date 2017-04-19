3898459586ca9d672f26264345a32d982a56eb38
Superbil <superbil@gmail.com>
2017-04-20 04:29:43 +0800

    New function executeShellScript

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -16,5 +16,22 @@ function reloadConfig(files)
 end
 local hammerspoon_watcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 
+function executeShellScript (filePath)
+   aslog = hs.logger.new('executeShellScript','debug')
+   -- Must change to string
+   local command = string.format('do shell script %q', filePath)
+   aslog.df(command)
+   ok,result,message = hs.osascript.applescript(command)
+   aslog.v(ok)
+   if result then
+      aslog.d('success ' .. result)
+   else
+      aslog.d('failed')
+      for key, val in pairs(message) do
+         print(key, val)
+      end
+   end
+end
+
 -- Everything is fine
 hs.alert.show("Hammerspoon Config loaded")
