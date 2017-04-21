be5d773f174a4103df47ea80de00c696fcc45456
Superbil <superbil@gmail.com>
2017-04-22 01:51:26 +0800

    Configure hammerspoon
    - Add some method
    
    - Add some real configuration
    
    - Setup grid style
    
    - Setup resize and nudge by grid

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,20 +1,59 @@
--- Reload config
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
-  hs.reload()
-end)
+-------------------------------------------------------------------------------
+-- real configuration
+-------------------------------------------------------------------------------
+-- variable config
+hs.window.animationDuration = 0
+hs.window.setShadows(false)
+local primaryScreen = hs.screen.primaryScreen()
+local secoundScreen = primaryScreen:toEast()
 
-function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
-end
-local hammerspoon_watcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+-- grid config
+hs.grid.setGrid('11x2', primaryScreen)
+hs.grid.setMargins(hs.geometry.size(0,0))
+hs.grid.setGrid('2x2', secoundScreen)
+
+-- keyboard modifiers for bindings
+local keys = {
+   ca  = {'ctrl', 'alt'        },
+   cac = {'cmd',  'alt', 'ctrl'}
+}
+
+-- Hotkeys
+hs.hotkey.bind({"ctrl"}, "escape", function() hs.grid.show() end)
+
+hs.hotkey.bind(keys.ca, "h", function() pushGrid('0,0 2x1', primaryScreen) end)         -- chat-left
+
+hs.hotkey.bind(keys.ca, "u", function() pushGrid('0,0 2x1', primaryScreen) end)         -- chat-t
+hs.hotkey.bind(keys.ca, "j", function () pushGrid('0,0 2x4', primaryScreen) end)        -- chat
+hs.hotkey.bind(keys.ca, "m", function () pushGrid('0,1 2x1', primaryScreen) end)        -- chat-b
+
+hs.hotkey.bind(keys.ca, "i", function() pushGrid('2,0 2x1', primaryScreen) end)         -- left2-t
+hs.hotkey.bind(keys.ca, "k", function () pushGrid('2,0 2x4', primaryScreen) end)        -- left2
+hs.hotkey.bind(keys.ca, ",", function () pushGrid('2,1 2x1', primaryScreen) end)        -- left2-b
+
+hs.hotkey.bind(keys.ca, "o", function () pushGrid('4,0 7x2', primaryScreen) end)        -- main
+hs.hotkey.bind(keys.ca, "l", function () pushGrid('2,0 9x2', primaryScreen) end)        -- bighMain
+
+hs.hotkey.bind(keys.ca, ".", function () pushScreen(primaryScreen,0.1,0.1,0.8,0.8) end) -- center
+
+hs.hotkey.bind(keys.ca, "n", function() pushScreen(primaryScreen,0,0,1,1) end)          -- full screen on screen 1
+hs.hotkey.bind(keys.ca, "p", function() pushScreen(secoundScreen,0,0,1,1) end)          -- full screen on screen 2
+
+-- Resize window by grid
+hs.hotkey.bind(keys.cac, "right", function() hs.grid.resizeWindowWider() end)
+hs.hotkey.bind(keys.cac, "left", function() hs.grid.resizeWindowThinner() end)
+hs.hotkey.bind(keys.cac, "up", function() hs.grid.resizeWindowShorter() end)
+hs.hotkey.bind(keys.cac, "down", function() hs.grid.resizeWindowTaller() end)
+
+-- Nudge window by grid
+hs.hotkey.bind(keys.ca, "right", function() hs.grid.pushWindowRight() end)
+hs.hotkey.bind(keys.ca, "left", function() hs.grid.pushWindowLeft() end)
+hs.hotkey.bind(keys.ca, "up", function() hs.grid.pushWindowUp() end)
+hs.hotkey.bind(keys.ca, "down", function() hs.grid.pushWindowDown() end)
+
+-------------------------------------------------------------------------------
+-- Functions
+-------------------------------------------------------------------------------
 
 function executeShellScript (filePath)
    aslog = hs.logger.new('executeShellScript','debug')
@@ -33,25 +72,71 @@ function executeShellScript (filePath)
    end
 end
 
---------------------------------------------------------------------------------
--- Main
---------------------------------------------------------------------------------
-
--- Full frame on screen 0
-hs.hotkey.bind({"ctrl", "alt"}, "n", function()
-  local win = hs.window.focusedWindow()
-  local screen = hs.screen'0,0'
-  local max = screen:frame()
-  win:setFrame(max)
+-- Reload config
+hs.hotkey.bind(keys.cac, "R", function()
+  hs.reload()
 end)
 
--- Full frame on screen 1
-hs.hotkey.bind({"ctrl", "alt"}, "p", function()
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+local hammerspoon_watcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+-------------------------------------------------------------------------------
+-- from https://github.com/exark/dotfiles/blob/master/.hammerspoon/init.lua
+-- Resize window for chunk of screen.
+-- For x and y: use 0 to expand fully in that dimension, 0.5 to expand halfway
+-- For w and h: use 1 for full, 0.5 for half
+-------------------------------------------------------------------------------
+function push(x, y, w, h)
   local win = hs.window.focusedWindow()
-  local screen = hs.screen'1,0'
+  local f = win:frame()
+  local screen = win:screen()
   local max = screen:frame()
-  win:setFrame(max)
-end)
+
+  f.x = max.x + (max.w*x)
+  f.y = max.y + (max.h*y)
+  f.w = max.w*w
+  f.h = max.h*h
+  win:setFrame(f)
+end
+
+function pushWindow(win, x, y, w, h)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w*x)
+  f.y = max.y + (max.h*y)
+  f.w = max.w*w
+  f.h = max.h*h
+  win:setFrame(f)
+end
+
+function pushScreen(screen, x, y, w, h)
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w*x)
+  f.y = max.y + (max.h*y)
+  f.w = max.w*w
+  f.h = max.h*h
+  win:setFrame(f)
+end
+
+function pushGrid(cell, screen)
+   local win = hs.window.focusedWindow()
+   hs.grid.set(win, cell, screen)
+end
 
 -- Everything is fine
 hs.alert.show("Hammerspoon Config loaded")
