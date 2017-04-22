e542efb55c33b25f3c7785f245686c6f6700ebe8
Superbil <superbil@gmail.com>
2017-04-22 15:54:43 +0800

    New hotkey to change wifi interface to AirplaneMode

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -61,6 +61,27 @@ hs.hotkey.bind(keys.c, "f1", function() hs.itunes.previous() end)
 -- Functions
 -------------------------------------------------------------------------------
 
+-- Airplane mode
+hs.hotkey.bind(keys.cac, "a", function() airplanMode() end)
+
+doAirplan = false
+function airplanMode()
+   local wifiFace = ''
+   if doAirplan then
+      wifiFace = 'Automatic'
+   else
+      wifiFace = 'AirplaneMode'
+   end
+
+   output, status, rtype, rc = hs.execute('scselect ' .. wifiFace)
+   if status then
+      hs.alert.show('Wifi -> ' .. wifiFace)
+      doAirplan = true
+   else
+      hs.alert.show('Change Wifi failed')
+   end
+end
+
 -- Reload config
 hs.hotkey.bind(keys.cac, "R", function()
   hs.reload()
