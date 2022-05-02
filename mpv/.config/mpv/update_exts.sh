77e5a751e3d01f9eac995da085fbdf2b17bd1bea
Superbil <superbil@gmail.com>
2022-05-02 15:24:30 +0800

    Add simple checker warnning duti isn't install

--- mpv/.config/mpv/update_exts.sh
+++ mpv/.config/mpv/update_exts.sh
@@ -2,8 +2,14 @@
 
 EXTS=( 3GP ASF AVI FLV M4V MKV MOV MP4 MPEG MPG MPG2 MPG4 RMVB WMV MTS WEBM )
 
-for ext in ${EXTS[@]}
-do
-    duti -vs io.mpv $ext all
-    duti -vs io.mpv $(echo $ext | tr "[:upper:]" "[:lower:]") all
-done
+if (( $+commands[duti] )); then
+
+    for ext in ${EXTS[@]}
+    do
+        duti -vs io.mpv $ext all
+        duti -vs io.mpv $(echo $ext | tr "[:upper:]" "[:lower:]") all
+    done
+
+else
+    echo "duti not install!"
+fi
