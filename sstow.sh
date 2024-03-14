fbe2c2efb1468e107133f54a88ecefbe2d06b04e
Superbil <superbil@gmail.com>
2024-03-14 17:00:00 +0800

    Fix wrong function at sstow

--- sstow.sh
+++ sstow.sh
@@ -8,7 +8,7 @@ for arg in "$@"
 do
     if [[ -d $arg ]]; then
         echo "Installing $arg"
-        _stow -t "$TARGET" $arg
+        $_stow -t "$TARGET" $arg
     else
         echo "$ARG don't existed won't install"
     fi
